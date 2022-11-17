ea77f0abe1ac79f3cd61353931031039dcf04dd7
Muhammad Hakaman Athhar <hakaman.athhar@moladin.com>
2022-11-17 10:32:20 +0000

    Merged in feat/add_date_time (pull request #22)
    add date time type
    
    * add date time type
    
    
    Approved-by: Adi Saripuloh
    Approved-by: Nur Jamil

--- time/time.go
+++ time/time.go
@@ -2,6 +2,9 @@
 package time
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"strings"
 	"time"
 )
 
@@ -23,4 +26,24 @@ func (t *timeStruct) Now() time.Time {
 
 func (t *timeStruct) ToDateTime() string {
 	return t.Now().In(time.Local).Format("2006-01-02 15:04:05")
+}
+
+type DateTime time.Time
+
+func (b *DateTime) UnmarshalJSON(bs []byte) error {
+	s := strings.Trim(string(bs), "\"")
+	t, err := time.ParseInLocation("2006-01-02 15:04:05", s, LoadTimeZoneAsiaJakarta())
+	if err != nil {
+		return err
+	}
+	*b = DateTime(t.UTC())
+	return nil
+}
+
+func (b DateTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(b).In(LoadTimeZoneAsiaJakarta()))
+}
+
+func (b DateTime) Value() (driver.Value, error) {
+	return json.Marshal(b)
 }
